7f7a2233198514030d76b2271db27101036d7b5d
ollulid-ui <ollulid@gmail.com>
2025-09-23 20:18:53 -0400

    Update database.py

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from .settings import settings
+from ..settings import settings
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {})
+engine = create_engine(
+    settings.DATABASE_URL,
+    connect_args={"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
